834f36bea3b2d1100293970dc0d9dfe94c8eeb0a
PORTATIL-ADRIAN\apena <adrianpcarnero@gmail.com>
2023-11-10 17:06:51 +0100

    10/11/2023

---
+++ PSP/MultiHilos/src/main/java/com/mycompany/multihilos/Mihilo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.multihilos;
+
+/**
+ *
+ * @author apena
+ */
+public class Mihilo {
+
+    public Mihilo(String nombre) {
+
+    }
+
+    
+    
+
+    public void run(){
+        
+        for(int i = 0;i<5 && !Thread.interrupted();i++){
+            
+            System.out.println("soy el hilo con nombre"+ Character.getName());
+            
+        ();
+        }
+            
+        
+        
+    }
+    
+}
