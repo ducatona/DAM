ca9afbadf6e028011a2f2c0a2be7143813ab8f65
PORTATIL-ADRIAN\apena <adrianpcarnero@gmail.com>
2023-12-01 00:20:17 +0100

    javafx

---
+++ DI/InicioSesionJavaFX/src/main/java/com/mycompany/iniciosesionjavafx/PrimaryController.java
@@ -0,0 +1,35 @@
+package com.mycompany.iniciosesionjavafx;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private Button btnInicioSesion, btnRegistrarse;
+    
+    
+    
+    @FXML
+    private StackPane containerForm;
+    
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @FXML
+    private void clicBtnInicioSesion(ActionEvent event) {
+        
+        
+        
+    }
+    }
---
+++ DI/InicioSesionJavaFX/src/main/java/com/mycompany/iniciosesionjavafx/SecondaryController.java
@@ -0,0 +1,25 @@
+package com.mycompany.iniciosesionjavafx;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SecondaryController {
+
+    @FXML
+    private Button secondaryButton;
+    @FXML
+    private Button miboton;
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+
+    @FXML
+    private void clickmiboton(ActionEvent event) {
+        
+        miboton.setText("asdgavsdsvhad");
+    }
+}
